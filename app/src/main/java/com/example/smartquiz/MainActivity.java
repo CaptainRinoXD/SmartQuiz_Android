e081b1fb28787614eebdcf4ae1e64e5743a50c1e
verylowpower <doudqwer@gmail.com>
2025-05-07 22:19:52 +0700

    change imgview to imgbutton

--- app/src/main/java/com/example/smartquiz/MainActivity.java
+++ app/src/main/java/com/example/smartquiz/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.smartquiz;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,38 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LinearLayout floatingMenu;
+    private ImageView menuButton;
+    private boolean isMenuVisible = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Áp insets cho layout
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // 🔽 Tìm view
+        floatingMenu = findViewById(R.id.floating_menu);
+        menuButton = findViewById(R.id.imageView); // đây là nút menu
+
+        // 🔽 Gắn sự kiện click
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isMenuVisible) {
+                    floatingMenu.setVisibility(View.GONE);
+                } else {
+                    floatingMenu.setVisibility(View.VISIBLE);
+                }
+                isMenuVisible = !isMenuVisible;
+            }
         });
     }
 }
